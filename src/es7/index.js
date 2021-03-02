1e8f152001e28e2f831c997289b961bba404dd2c
luismimultimedia <lgonzalez@serempre.com>
2021-03-01 21:47:24 -0500

    feat: Add new es features

---
+++ src/es7/index.js
@@ -0,0 +1,17 @@
+// includes
+
+let numbers = [1,2,3,6,7,8]
+
+if (numbers.includes(5)) {
+    console.log("incluye el numero")
+} else {
+    console.log("No incluye el numero")
+}
+
+// potenias exponenciales
+
+let base = 4;
+let exponent = 4;
+let result = base ** exponent;
+
+console.log(result)
